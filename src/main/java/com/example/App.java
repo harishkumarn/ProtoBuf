ec74eac2adc1df089961a29e79edd9c8f6967e01
Harish Kumar Natarajan <harishkr.coc@gmail.com>
2023-01-04 22:41:09 +0530

    optional params in proto

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,14 @@
 package com.example;
 import com.harish.proto.Employee; 
 
+import org.json.JSONObject;
+
 public class App 
 {
     public static void main( String[] args )
     {
+
+
 
 	    Employee emp = Employee.newBuilder()
             .setName("Harish Kumar")
@@ -13,5 +17,15 @@ public class App
             .setRole("MTS")
             .build();
         System.out.println( emp);
+
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("name", "Harish Kumar");
+        jsonObject.put("age", 25);
+        jsonObject.put("dob", "12-01-1998");
+        jsonObject.put("role", "MTS");
+
+        System.out.println( jsonObject);
+        
     }
 }
