b21af2da8b52b6d5b26edf4bef43a2f56638b11d
Harish Kumar Natarajan <harishkr.coc@gmail.com>
2023-01-14 17:21:02 +0530

    Server streaming

--- grpc-basics/src/main/java/com/grpc/App.java
+++ grpc-basics/src/main/java/com/grpc/App.java
@@ -9,6 +9,7 @@ import io.grpc.stub.StreamObserver;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+
 public class App extends ServerGrpc.ServerImplBase
 {
     @Override
@@ -23,4 +24,22 @@ public class App extends ServerGrpc.ServerImplBase
         responseObserver.onCompleted();
 
     }
+
+    @Override
+    public void getGreetings(Name requName, StreamObserver<Greet> responseObserver) {
+        String[] arr = new String[]{"Vanakkam ", "Namaskaram ","Namaste ", "Hello ","Adios "};
+        for(String greet : arr){
+            try{
+            Greet newGreet = Greet.newBuilder()
+                                .setGreeting(greet +requName.getName() )
+                                .setCurrentTime(LocalDate.now().toString() + " " + LocalTime.now().toString())
+                                .build();
+            
+            responseObserver.onNext(newGreet);
+            Thread.sleep(1000);
+            }catch(Exception e){}
+        }
+        responseObserver.onCompleted();
+    } 
+   
 }
