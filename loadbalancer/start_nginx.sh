f92cd7a0f68bc18cccbcbc710df039f78c1be9e8
Harish Kumar Natarajan <harishkr.coc@gmail.com>
2023-03-05 20:56:48 +0530

    nginx changes

--- loadbalancer/start_nginx.sh
+++ loadbalancer/start_nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 a=$(pwd)/conf/
-docker container run --name proxy   -p 8585:8585 -v $a:/etc/nginx/conf.d --rm nginx
+docker container run --name proxy   -p 8585:8585 -v $a:/etc/nginx/conf.d --rm nginx:1.15-alpine
